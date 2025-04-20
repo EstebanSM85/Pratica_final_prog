22c218dee6881efc6a0ebdf5eaab4b70bd1ee144
EstebanSM85 <estebansm85@gmail.com>
2025-04-20 19:50:17 +0200

    CLase efectivo terminada

--- src/practica/Efectivo.java
+++ src/practica/Efectivo.java
@@ -1,5 +1,8 @@
 package practica;
 
+import java.util.Map;
+import java.util.TreeMap;
+
 public class Efectivo extends PasarelaPago {
 	//Atributos propios
 	 private double entrega;
@@ -26,9 +29,34 @@ public class Efectivo extends PasarelaPago {
 		this.cambio = entrega-this.getImporte();
 	}
 	
+	public TreeMap<Double, Integer> calcularCambio() {
+		TreeMap<Double, Integer> cambioDesglose = new TreeMap<>();
+	    double[] valores = {200, 100, 50, 20, 10, 5, 2, 1, 0.5, 0.2, 0.1, 0.05, 0.02, 0.01};
+	    double restante = cambio;
+
+	    for (double valor : valores) {
+	        int cantidad = (int) (restante / valor);
+	        if (cantidad > 0) {
+	            cambioDesglose.put(valor, cantidad);
+	            restante -= cantidad * valor;
+	            restante = Math.round(restante * 100.0) / 100.0; // Redondeo para precisión.
+	        }
+	    }
+	    for (Map.Entry<Double, Integer> entry : cambioDesglose.entrySet()) {
+	        System.out.println(entry.getValue() + " --> " + entry.getKey() + "€");
+	    }
+
+	    return cambioDesglose;
+	}
+
 	@Override
     public boolean procesarPago() {
         System.out.println("Pago en efectivo registrado. Importe: " + getImporte() + "€");
+        if (cambio>0) {
+        	calcularCambio();	
+        }else {
+        	System.out.println("Entrega correcta, no se ha de devolver.");
+        }
         return true;
     }
 }
